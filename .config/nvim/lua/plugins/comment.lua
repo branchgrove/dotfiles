88bb3c0456920fcf09350dd7c20a41769ce17b80
Elias Lundgren <agile.oil1780@fastmail.com>
2024-03-09 00:04:20 +0100

    Add mini stuff

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,28 +1,10 @@
 return {
-  "numToStr/Comment.nvim",
-  lazy = false,
+  "echasnovski/mini.comment",
   opts = {
     mappings = {
-      basic = false,
-      extra = false,
-    },
-  },
-  config = function (plugin, opts)
-    require("Comment").setup(opts)
-    local comment = require("Comment.api")
-
-    vim.keymap.set(
-      "n",
-      "<Leader>/",
-      function() comment.toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end,
-      { desc = "Toggle comment line", }
-    )
-
-    vim.keymap.set(
-      "v",
-      "<Leader>/",
-      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-      { desc = "Toggle comment for selection" }
-    )
-  end
+      comment = "<Leader>/",
+      comment_line = "<Leader>/",
+      comment_visual = "<Leader>/"
+    }
+  }
 }
